7c04d5ea541a40ad6ae69f1dcbbda3d79484571f
MikeyGloriani <MikeyGloriani@users.noreply.github.com>
2022-11-15 18:50:59 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ if (navigator.serviceWorker) {
 }
 
 // returns a random integer from 1 to 6
-const randomNumber = Math.floor(Math.random() * 6) + 1;
+const randomNumber = Math.floor(Math.random() * 6) + 1
 
 /**
  * This function updates the slider value.
@@ -26,11 +26,10 @@ function updateSliderValue(valueFromSlider) {
 // process
 if (valueFromSlider == randomNumber) {
   document.getElementById("answer").innerHTML =
-  "That's right, I was thinking of number" + randomNumber
+    "That's right, I was thinking of number" + randomNumber
 }
 
 // if the guess is incorrect
 if (valueFromSlider == randomNumber) {
-  document.getElementById("answer").innerHTML =
-  "Wrong, try again" 
+  document.getElementById("answer").innerHTML = "Wrong, try again"
 }
